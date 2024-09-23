020c1bfdb3551799548e1d7001ae93b16a8d13d8
skye-lopez <sky.lopez.dev@gmail.com>
2024-09-22 22:43:43 -0700

    Commenting and small cleanup

--- limiter.go
+++ limiter.go
@@ -1,7 +1,6 @@
 package limiter
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
@@ -37,17 +36,17 @@ func NewLimiter(rate time.Duration, limit int, burst int) *Limiter {
 func (l *Limiter) Allowed(ip string) int {
 	l.mu.Lock()
 	defer l.mu.Unlock()
+	now := time.Now()
 
 	ctx, ok := l.IPMap[ip]
 	// If ip doesnt exist create and allow
 	if !ok {
-		now := time.Now()
 		nextTime := now.Add(l.Rate)
 		l.IPMap[ip] = &IPCtx{
 			LastTime: time.Now(),
 			NextTime: nextTime,
 			Amount:   l.Limit,
-			Burst:    l.Burst,
+			Burst:    l.Burst - 1,
 		}
 		return 200
 	}
@@ -58,24 +57,25 @@ func (l *Limiter) Allowed(ip string) int {
 		return 200
 	}
 
-	now := time.Now()
+	// If the nextTime interval has passed, reset
 	if ctx.NextTime.Unix() < now.Unix() {
 		nextTime := now.Add(l.Rate)
 		ctx.LastTime = now
 		ctx.NextTime = nextTime
 		ctx.Amount = l.Limit
-		ctx.Burst = l.Burst // TODO: Do we want to reset the burst?
+		ctx.Burst = l.Burst - 1
 
 		return 200
 	}
 
+	// If we are within the interval span and still have remaining requests, allow
 	if ctx.NextTime.Unix() > now.Unix() && ctx.Amount > 0 {
 		ctx.Amount -= 1
 		return 200
 	}
 
+	// Otherwise reject.
 	if ctx.NextTime.Unix() > now.Unix() && ctx.Amount <= 0 {
-		fmt.Println("nextTime is greater then the current time and amount is 0", ip, ctx.Amount)
 		return 429
 	}
 
